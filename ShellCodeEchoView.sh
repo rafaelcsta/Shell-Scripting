569ae1facb688644068fa944fb25b98438ae93bf
BlackCyber21 <35323386+BlackCyber21@users.noreply.github.com>
2021-08-25 03:15:54 -0300

    Add files via upload

---
+++ ShellCodeEchoView.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "Obtenha sua string literal em https://defuse.ca/online-x86-assembler.htm"
+echo
+echo "Cole o conteudo de seu shellcode em ./zz.txt: "
+echo
+echo "Abaixo sua output: "
+echo
+a=$(cat shellcode.txt)
+echo -e $a
+echo
